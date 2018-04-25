7d639e50853ce90b0200f55fbf2a1418516ae042
Zac Willmington <zac.willmington@gmail.com>
2018-04-25 20:09:52 +0000

    Done.

--- Rakefile
+++ Rakefile
@@ -1,6 +1,8 @@
 require_relative './config/environment'
 require 'sinatra/activerecord/rake'
 
+
+
 task :console do
   require 'irb'
   ARGV.clear
--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,21 @@ Bundler.require
 
 
 # put the code to connect to the database here
+ActiveRecord::Base.establish_connection(
+:adapter  => "sqlite3",
+:database => "db/artists.sqlite"
+)
 
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  genre TEXT,
+  age INTEGER,
+  hometown TEXT
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
 
 require_relative "../artist.rb"
