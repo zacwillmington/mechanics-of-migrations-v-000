72d1576dcece808b3bb4a3f97b674bcd69b5c652
Zac Willmington <zac.willmington@gmail.com>
2018-04-25 20:40:23 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -15,15 +15,15 @@ ActiveRecord::Base.establish_connection(
 :database => "db/artists.sqlite"
 )
 
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT,
-  age INTEGER,
-  hometown TEXT
-  )
-SQL
+# sql = <<-SQL
+#   CREATE TABLE IF NOT EXISTS artists (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT,
+#   genre TEXT,
+#   age INTEGER,
+#   hometown TEXT
+#   )
+# SQL
 
 ActiveRecord::Base.connection.execute(sql)
 
